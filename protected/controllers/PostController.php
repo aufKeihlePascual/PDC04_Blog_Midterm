143685d71929e1b096f0048f1adadbab582e8882
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-03-19 00:26:31 +0800

    Updated actionView to include comment, and newComment function

--- protected/controllers/PostController.php
+++ protected/controllers/PostController.php
@@ -62,9 +62,28 @@ class PostController extends Controller
 	public function actionView()
 	{
 		$post=$this->loadModel();
+		$comment=$this->newComment($post);
+	
 		$this->render('view',array(
 			'model'=>$post,
+			'comment'=>$comment,
 		));
+	}
+
+	protected function newComment($post)
+	{
+		$comment=new Comment;
+		if(isset($_POST['Comment']))
+		{
+			$comment->attributes=$_POST['Comment'];
+			if($post->addComment($comment))
+			{
+				if($comment->status==Comment::STATUS_PENDING)
+					Yii::app()->user->setFlash('commentSubmitted','Thank you for your comment. Your comment will be posted once it is approved.');
+				$this->refresh();
+			}
+		}
+		return $comment;
 	}
 
 	/**
