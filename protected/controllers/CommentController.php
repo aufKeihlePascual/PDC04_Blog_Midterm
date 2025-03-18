ad5dea29b7b7309e69557c0041304f910a1d21f0
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-03-19 00:47:52 +0800

    Updated actionIndex in Comment Controller to make comments needing approval come up first

--- protected/controllers/CommentController.php
+++ protected/controllers/CommentController.php
@@ -124,7 +124,13 @@ class CommentController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$dataProvider=new CActiveDataProvider('Comment');
+		$dataProvider=new CActiveDataProvider('Comment', array(
+			'criteria'=>array(
+				'with'=>'post',
+				'order'=>'t.status, t.create_time DESC',
+			),
+		));
+	
 		$this->render('index',array(
 			'dataProvider'=>$dataProvider,
 		));
