fff2617cb893cc7149ce2240f0761f91f33319a9
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-03-21 00:43:44 +0800

    Designed Post index, view, and _view

--- protected/views/post/_view.php
+++ protected/views/post/_view.php
@@ -3,41 +3,32 @@
 /* @var $data Post */
 ?>
 
-<div class="view">
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
-	<?php echo CHtml::encode($data->title); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('content')); ?>:</b>
-	<?php echo CHtml::encode($data->content); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('tags')); ?>:</b>
-	<?php echo CHtml::encode($data->tags); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('status')); ?>:</b>
-	<?php echo CHtml::encode(Lookup::items('PostStatus')[$data->status]); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('create_time')); ?>:</b>
-	<?php echo CHtml::encode(date('F j, Y h:i:s A', $data->create_time)); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('update_time')); ?>:</b>
-	<?php echo CHtml::encode(date('F j, Y h:i:s A', $data->update_time)); ?>
-	<br />
-
-	<?php /*
-	<b><?php echo CHtml::encode($data->getAttributeLabel('author_id')); ?>:</b>
-	<?php echo CHtml::encode($data->author_id); ?>
-	<br />
-
-	*/ ?>
-
+<div class="blog-post">
+  <!-- Post Title -->
+  <h2>
+    <?php echo CHtml::link(CHtml::encode($data->title), array('view', 'id'=>$data->id)); ?>
+  </h2>
+  
+  <!-- Post Meta: Date and Tags -->
+  <div class="post-meta">
+    <span class="post-date"><?php echo date('F j, Y', $data->create_time); ?></span>
+    <?php if (!empty($data->tags)): ?>
+      <span class="post-tags"> | Tags: <?php echo CHtml::encode($data->tags); ?></span>
+    <?php endif; ?>
+  </div>
+  
+  <!-- Post Excerpt -->
+  <div class="post-excerpt">
+    <?php 
+      // Remove HTML tags for a clean summary and limit to 250 characters
+      $plainText = strip_tags($data->content);
+      $summary = strlen($plainText) > 250 ? substr($plainText, 0, 250) . '...' : $plainText;
+      echo CHtml::encode($summary);
+    ?>
+  </div>
+  
+  <!-- Read More Link -->
+  <div class="read-more">
+    <?php echo CHtml::link('Read More', array('view', 'id'=>$data->id)); ?>
+  </div>
 </div>
