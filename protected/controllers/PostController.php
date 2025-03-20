d972e989055eb418bbf3f1a37a639cf6b8ce5ed7
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-03-20 21:43:38 +0800

    Added demo user to list of users with access to admin and delete actions

--- protected/controllers/PostController.php
+++ protected/controllers/PostController.php
@@ -38,7 +38,7 @@ class PostController extends Controller
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				'users'=>array('admin', 'demo'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
